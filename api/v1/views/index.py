d392ad3d846defbaed786a727d7ef12483b89afb
flourishcodes <kunle@adeniran.com>
2018-08-20 23:25:42 -0700

    Created app.py and index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Create status return"""
-    return jsonify({"status": "ok"})
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def stats():
+    """Returns the classes with their instance counts"""
+    stat = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stat)
