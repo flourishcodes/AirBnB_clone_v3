68ef752f5bf339903d600aa0c89c5ecf7469137b
adrielt07 <adrieltolentino@outlook.com>
2018-08-21 23:59:05 +0000

    added a forward slash in create_state method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -22,10 +22,10 @@ def all_states(state_id=None):
         abort(404)
 
 
-@app_views.route('/states', methods=['POST'])
+@app_views.route('/states/', methods=['POST'])
 def create_state():
     '''Creates an instance of State and save it to storage'''
-    form = request.get_json(force=True)
+    form = request.get_json()
     if 'name' not in form:
         abort(400)
         return jsonify({"error": "Missing Name"})
@@ -46,3 +46,15 @@ def delete_state(state_id):
         abort(404)
         return jsonify({})
     return jsonify({})
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    '''Updates State object attribute'''
+    try:
+        state_obj = storage.get('State', state_id)
+        form = request.get_json(force=True)
+        for k, v in form.items():
+            setattr(state_obj, k, v)
+        return jsonify(state_obj.to_dict(), 200)
+    except:
+        abort(404)
