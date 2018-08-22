439315d9dcf141ca2e1b3cbfbe80a03763249e36
adrielt07 <adrieltolentino@outlook.com>
2018-08-22 22:44:33 +0000

    added docstring for module

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''
+routes:
+/status - returns status: Ok
+/stats - returns the number of instance
+'''
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
