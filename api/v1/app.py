4122b5fc76aae011f05f115e7730892c0982859d
adrielt07 <adrieltolentino@outlook.com>
2018-08-23 00:22:05 +0000

    removed the import and corrected CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,15 +2,14 @@
 """Python api built with flask"""
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-import models
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -24,12 +23,12 @@ def error_handler(error):
     '''Returns a JSON formatted 404 status code'''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-"""
+
 @app.errorhandler(400)
 def error_handler2(error):
     '''Returns a JSON formatted 404 status code'''
     return make_response(jsonify({'error': 'Not a JSON'}), 400)
-"""
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
